a15c1713ec02954abaae6547400539f90283897d
Sunmeet Singh <renu.kabeer@gmail.com>
2022-10-16 16:34:36 +0530

    sd

--- database/db.js
+++ database/db.js
@@ -1,7 +1,7 @@
 let Pool = require("pg").Pool
 const pool = new Pool({
     user: "postgres",
-    host: "localhost",
+    host: "express-server1.vercel.app",
     database: "standupurls",
     password: "sunmeet",
     port: 5432
