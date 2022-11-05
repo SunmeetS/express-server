f9d47110d1d6e2820c2040f3188eebaec882a387
Sunmeet Singh <renu.kabeer@gmail.com>
2022-11-05 14:49:21 +0530

    working or not

--- database/db.js
+++ database/db.js
@@ -1,10 +1,10 @@
 require('dotenv').config({path:'./.env'});
 let Pool = require("pg").Pool
 const pool = new Pool({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASSWORD,
+    user: "qvkfibib",
+    host: "satao.db.elephantsql.com",
+    database: "qvkfibib",
+    password: "wuXFh1DyeXBE29lXToXk-_bK_gimeXWY",
     port: 5432,
     max: 1,
 })
