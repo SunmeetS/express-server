91c7ced8a66b4b3927011ea85675741f56a92871
Sunmeet Singh <renu.kabeer@gmail.com>
2022-11-05 14:55:45 +0530

    working or not

--- database/db.js
+++ database/db.js
@@ -1,13 +1,15 @@
 require('dotenv').config({path:'./.env'});
 let Pool = require("pg").Pool
 const pool = new Pool({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASSWORD,
+    user: ""+process.env.DB_USER+"",
+    host: ""+process.env.DB_HOST+"",
+    database: ""+process.env.DB_NAME+"",
+    password: ""+process.env.DB_PASSWORD+"",
     port: 5432,
     max: 1,
 })
+
+console.log(process.env.DB_USER, process.env.DB_HOST, process.env.DB_NAME, process.env.DB_PASSWORD)
 
 module.exports = {
     pool
